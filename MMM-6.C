226fbb900b0b04e2bce4c26b2c5463155867d726
mynklathiya <132878852+mynklathiya@users.noreply.github.com>
2023-05-09 10:35:17 +0530

    Add files via upload

---
+++ MMM-6.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+int radius;
+float area;
+
+printf("enter radius of circle=");
+scanf("%d",&radius);
+area=3.14*radius*radius;
+printf("area of perimeter circle=");
+
+getch();
+
+}
